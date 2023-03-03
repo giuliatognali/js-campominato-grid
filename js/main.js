dcd63f57f558758960a7213e4cd12f826de3899c
giuliatognali <giuliatognali@gmail.com>
2023-03-04 00:20:20 +0100

    100 celle

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
 'use strict';
+
+/************ 
+ * Function
+ ************/
+function mycreateElement(elementHTML, className, ValueHTML) {
+    const element = document.createElement(elementHTML);
+    element.classList.add(className);
+    element.innerText = ValueHTML;
+    return element;
+}
+
+function myAppendElement(containerHTML, elementHTML) {
+    containerHTML.append(elementHTML);
+}
+
+
+/************ 
+ * Main
+ ************/
+
+
+const containerBoard = document.querySelector('.board');
+
+const playBtn =document.getElementById ('play');
+
+let cellNumber = 100; // TODO cambiare in base al livello
+
+
+playBtn.addEventListener('click',
+function(){
+    
+for (let i = 1; i <= cellNumber; i++) {
+    
+    const myCell = mycreateElement('div', 'cell', i);
+    myAppendElement(containerBoard, myCell);
+
+    myCell.addEventListener('click',
+        function () {
+            myCell.classList.add('blue');
+        }
+    )
+}
+
+})
